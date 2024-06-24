926acaaa49655ddbff8de81b81827fafe7b1fa9b
Siraaj Khandkar <siraaj@khandkar.net>
2024-06-24 01:08:13 -0400

    Test the ps module

--- src/ps.rs
+++ src/ps.rs
@@ -2,7 +2,7 @@ use std::collections::{HashMap, HashSet};
 
 use anyhow::{anyhow, bail, Context};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Info {
     pub pid: u32,
     pub ppid: u32,
@@ -10,7 +10,15 @@ pub struct Info {
 }
 
 pub async fn list() -> anyhow::Result<Vec<Info>> {
-    let out = exec("ps", &["-eo", "pid,ppid,pgrp"]).await?;
+    let out = ps_exec().await?;
+    ps_parse(&out)
+}
+
+async fn ps_exec() -> anyhow::Result<String> {
+    exec("ps", &["-eo", "pid,ppid,pgrp"]).await
+}
+
+fn ps_parse(out: &str) -> anyhow::Result<Vec<Info>> {
     let mut list = Vec::new();
     // Skip headers.
     for line in out.lines().skip(1) {
@@ -32,16 +40,16 @@ pub async fn list() -> anyhow::Result<Vec<Info>> {
 }
 
 pub fn groups(procs: &[Info]) -> HashMap<u32, HashSet<u32>> {
-    let mut groups: HashMap<u32, HashSet<u32>> = HashMap::new();
+    let mut pgroup2pids: HashMap<u32, HashSet<u32>> = HashMap::new();
     for proc in procs {
-        groups
+        pgroup2pids
             .entry(proc.pgrp)
             .and_modify(|group| {
                 group.insert(proc.pid);
             })
             .or_insert(HashSet::from([proc.pid]));
     }
-    groups
+    pgroup2pids
 }
 
 pub fn children(procs: &[Info]) -> HashMap<u32, HashSet<u32>> {
@@ -83,4 +91,94 @@ async fn exec(cmd: &str, args: &[&str]) -> anyhow::Result<String> {
                 String::from_utf8_lossy(stderr.as_slice())
             )
         })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const OUT_0: &str = "PID PPID PGRP";
+    const OUT_1: &str = "  PID  PPID  PGRP
+    1     0     1
+    2     1     2
+    3     1     3
+    4     1     4
+    5     4     4
+";
+
+    #[test]
+    fn test_0_1_parse() {
+        assert!(ps_parse(OUT_0).unwrap().is_empty());
+    }
+
+    #[test]
+    fn test_0_2_process_groups() {
+        assert!(groups(ps_parse(OUT_0).unwrap().as_slice()).is_empty());
+    }
+
+    #[test]
+    fn test_0_3_children() {
+        assert!(children(ps_parse(OUT_0).unwrap().as_slice()).is_empty());
+    }
+
+    #[test]
+    fn test_1_1_parse() {
+        let out = OUT_1;
+        let list_expected = vec![
+            Info {
+                pid: 1,
+                ppid: 0,
+                pgrp: 1,
+            },
+            Info {
+                pid: 2,
+                ppid: 1,
+                pgrp: 2,
+            },
+            Info {
+                pid: 3,
+                ppid: 1,
+                pgrp: 3,
+            },
+            Info {
+                pid: 4,
+                ppid: 1,
+                pgrp: 4,
+            },
+            Info {
+                pid: 5,
+                ppid: 4,
+                pgrp: 4,
+            },
+        ];
+        let list_actual = ps_parse(out).unwrap();
+        assert_eq!(list_expected, list_actual);
+    }
+
+    #[test]
+    fn test_1_2_process_groups() {
+        let out = OUT_1;
+        let groups_expected = HashMap::from([
+            (1, HashSet::from([1])),
+            (2, HashSet::from([2])),
+            (3, HashSet::from([3])),
+            (4, HashSet::from([4, 5])),
+        ]);
+        let list = ps_parse(out).unwrap();
+        let groups_actual = groups(&list[..]);
+        assert_eq!(groups_expected, groups_actual);
+    }
+
+    #[test]
+    fn test_1_3_children() {
+        let out = OUT_1;
+        let children_expected = HashMap::from([
+            (0, HashSet::from([1])),
+            (1, HashSet::from([2, 3, 4])),
+            (4, HashSet::from([5])),
+        ]);
+        let list = ps_parse(out).unwrap();
+        let children_actual = children(&list[..]);
+        assert_eq!(children_expected, children_actual);
+    }
 }
